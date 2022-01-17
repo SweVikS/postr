e4d3ccc7194550bc833e9875ab74c3481a108cf1
SweVikS <viktor.salmberg@gmail.com>
2022-01-17 18:57:35 +0100

    Added Controllers/html for signin/signup page. Refactored with Thymeleaf

--- src/main/kotlin/app/postr/controllers/HomeController.kt
+++ src/main/kotlin/app/postr/controllers/HomeController.kt
@@ -1,12 +1,21 @@
 package app.postr.controllers
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class HomeController {
 
-    @GetMapping("/")
+
+    //List of strings added
+//    @GetMapping("home")
+//    fun Home(model : Model) : String {
+//        model.addAttribute("postList", listOf("Hej", "på", "dig"))
+//        return "home"
+//    }
+
+    @GetMapping("home")
     fun Home() : String {
         return "home"
     }
